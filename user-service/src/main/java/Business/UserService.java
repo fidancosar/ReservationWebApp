17e6bfe9bf72e0a814dac8f1425ee04060484f56
fidancosar <141564627+fidancosar@users.noreply.github.com>
2024-07-21 20:43:19 +0300

    create user

--- user-service/src/main/java/Business/UserService.java
+++ user-service/src/main/java/Business/UserService.java
@@ -1,5 +1,7 @@
 package Business;
 
+import Client.ReservationServiceClient;
+import Dto.AddReservationRequest;
 import Dto.UserDto;
 import Model.User;
 import Repository.UserRepository;
@@ -10,6 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final ReservationServiceClient reservationServiceClient;
+
+    public UserService(UserRepository userRepository,
+                       ReservationServiceClient reservationServiceClient) {
+        this.userRepository = userRepository;
+        this.reservationServiceClient = reservationServiceClient;
+    }
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -20,12 +29,21 @@ public class UserService {
                 .orElseThrow(() -> new UserNotFoundException("User could not found by user id: " + userId));
 
         UserDto userDto = new UserDto(user.getUserId());
+            user.getUserReservation()
+                    .stream()
+                    .map(reservationServiceClient::getReservationById)
+                    .map(ResponseEntity::getBody)
+                    .collect(Collectors.toList());
         return userDto;
     }
 
     public UserDto createUser() {
         User newUser = userRepository.save(new User());
         return new UserDto(newUser.getUserId());
+    }
+
+    public void addReservation(AddReservationRequest request){
+        Integer
     }
 
 }
